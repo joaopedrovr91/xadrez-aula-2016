8ed0c4c7589d57649ac84c14109a1600727f1407
Joao Pedro VR <joaopedrovr91@gmail.com>
2016-09-26 09:21:37 -0300

    Primeiro commit

---
+++ xadrez_aula/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace xadrez_aula
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            Console.Write("Digite um valor inteiro: ");
+
+            int x = int.Parse(Console.ReadLine());
+
+            int y = 2 * x;
+
+            Console.WriteLine("DOBRO = " + y);
+
+            Console.ReadLine();
+        }
+    }
+}
